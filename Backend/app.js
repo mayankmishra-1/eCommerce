068bff17c1a536fe9697aa0bdc9294f331a0707d
Mayank <mayankkumarmishra1@gmail.com>
2026-03-06 09:09:07 +0530

    packages

---
+++ Backend/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+
+// import connectDB from "./config/db.js";
+
+// import productRoutes from "./routes/productRoutes.js";
+// import cartRoutes from "./routes/cartRoutes.js";
+// import orderRoutes from "./routes/orderRoutes.js";
+
+// import "./utils/cartExpiryJob.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+// connectDB();
+
+// app.use("/api", productRoutes);
+// app.use("/api", cartRoutes);
+// app.use("/api", orderRoutes);
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
