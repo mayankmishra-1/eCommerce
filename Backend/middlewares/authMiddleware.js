ef362d00635a8f058a2fdb5816195c4026e748e3
Mayank <mayankkumarmishra1@gmail.com>
2026-03-06 09:13:40 +0530

    RBAC

---
+++ Backend/middlewares/authMiddleware.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+
+export default function authenticate(req, res, next) {
+
+    const token = req.headers.authorization;
+
+    if (!token) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    try {
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.user = decoded;
+
+        next();
+
+    } catch (err) {
+        return res.status(401).json({ message: "Invalid Token" });
+    }
+}
---
+++ Backend/middlewares/roleMiddleware.js
@@ -0,0 +1,11 @@
+export const allowRole = (role) => {
+
+    return (req, res, next) => {
+
+        if (req.user.role !== role) {
+            return res.status(403).json({ message: "Forbidden" });
+        }
+
+        next();
+    };
+};
